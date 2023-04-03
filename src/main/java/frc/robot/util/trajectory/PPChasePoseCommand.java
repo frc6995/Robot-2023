593a0b17568e3a5c27dd46a5d9287e5186bb5b5b
shueja-personal <j.a.shue@outlook.com>
2023-04-02 18:11:14 -0700

    End of LVR

--- src/main/java/frc/robot/util/trajectory/PPChasePoseCommand.java
+++ src/main/java/frc/robot/util/trajectory/PPChasePoseCommand.java
@@ -84,7 +84,7 @@ public class PPChasePoseCommand extends CommandBase implements Loggable {
         m_outputChassisSpeedsRobotRelative = outputChassisSpeedsFieldRelative;
         m_drive = drive;
         m_finishTrigger = new Trigger(()->m_pose.get().getTranslation().getDistance(m_targetPose.get().getTranslation()) < Units.inchesToMeters(1)
-    && Math.abs(m_pose.get().getRotation().getDegrees() - m_targetPose.get().getRotation().getDegrees()) < 1)
+    && Math.abs(m_pose.get().getRotation().getDegrees() - m_targetPose.get().getRotation().getDegrees()) < 0.75)
     .debounce(0.05);
         addRequirements(m_drive);
         }
