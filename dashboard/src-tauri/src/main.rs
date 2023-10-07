8482de30ebffc7f502bce44f0af031e19f08035c
shueja-personal <j.a.shue@outlook.com>
2023-10-07 14:48:32 -0700

    dashboard fixes

--- dashboard/src-tauri/src/main.rs
+++ dashboard/src-tauri/src/main.rs
@@ -19,24 +19,23 @@ fn main() {
   // context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
 
   tauri::Builder::default()
-    //.plugin(tauri_plugin_localhost::Builder::new(port).build())
-    // .setup(move |app| {
-    //   let operator = WindowBuilder::new(
-    //     app,
-    //     "operator",
-    //     WindowUrl::External(format!("http://localhost:{}", port).parse().unwrap())
-    //   )
-    //   .title("Operator")
-    //   .build()?;
-    //   let main = WindowBuilder::new(
-    //     app,
-    //     "main",
-    //     WindowUrl::External(format!("http://localhost:{}/main", port).parse().unwrap())
-    //   )
-    //   .title("Main")
-    //   .build()?;
-    //   Ok(())
-    // })
+    .setup(move |app| {
+      let operator = WindowBuilder::new(
+        app,
+        "operator",
+        WindowUrl::App("/".into())
+      )
+      .title("Operator")
+      .build()?;
+      let main = WindowBuilder::new(
+        app,
+        "main",
+        WindowUrl::App("/main".into())
+      )
+      .title("Main")
+      .build()?;
+      Ok(())
+    })
     .run(context)
     .expect("error while running tauri application");
 }
